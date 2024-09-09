380023884ac44eacde754375f2178e8ec1e30a69
salihandro07 <khamrayumovsalih2gmail.com>
2024-09-09 11:24:43 +0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> listA = new ArrayList<>();
+        System.out.println("Введите 5 строк для списка A:");
+        for (int i = 0; i < 5; i++) {
+            listA.add(scanner.nextLine());
+        }
+
+        System.out.println("Список A: " + listA);
+
+        List<String> listB = new ArrayList<>();
+        System.out.println("Введите 5 строк для списка B:");
+        for (int i = 0; i < 5; i++) {
+            listB.add(scanner.nextLine());
+        }
+
+        System.out.println("Список B: " + listB);
+
+        List<String> listC = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            listC.add(listA.get(i));
+            listC.add(listB.get(4 - i));
+        }
+
+        System.out.println("Список C после объединения: " + listC);
+        Collections.sort(listC, Comparator.comparingInt(String::length));
+        System.out.println("Список C после сортировки по длине строк: " + listC);
     }
 }
